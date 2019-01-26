58dd6d0f61b2cd1a616aa907fa22305113db492d
Brandon B <bruceb888@gmail.com>
2019-01-26 02:00:30 -0500

    Adding simple camera script

---
+++ Home Life Balance/Assets/Scripts/FollowHighestObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FollowHighestObject : MonoBehaviour
+{
+    public string tag;
+    public float cameraSpeed;
+    private Vector3 newCameraPos;
+    private GameObject highestGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        highestGameObject = GameObject.FindGameObjectsWithTag(tag).OrderBy(highestGameObject => highestGameObject.transform.position.y).Last();
+        newCameraPos = new Vector3(highestGameObject.transform.position.x, highestGameObject.transform.position.y, transform.position.z);
+        this.transform.position = Vector3.Lerp(transform.position, newCameraPos, cameraSpeed * Time.deltaTime);
+    }
+}
