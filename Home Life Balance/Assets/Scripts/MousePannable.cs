34c529239415a7fa389319e33d92f04d7193e338
Brandon B <bruceb888@gmail.com>
2019-01-26 13:39:14 -0500

    Updated panning script to disable when an unplaced block is available

--- Home Life Balance/Assets/Scripts/MousePannable.cs
+++ Home Life Balance/Assets/Scripts/MousePannable.cs
@@ -6,6 +6,7 @@ public class MousePannable : MonoBehaviour
 {
     public float sensitivity;
     private Vector3 lastCameraPosition;
+    public string tagThatBlocksPanning = "UnplacedBlock";
 
     // Start is called before the first frame update
     void Start()
@@ -16,16 +17,18 @@ public class MousePannable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-        {
-            lastCameraPosition = Input.mousePosition;
-        }
+        if (GameObject.FindGameObjectsWithTag(tagThatBlocksPanning).Length == 0) {
+            if (Input.GetMouseButtonDown(1))
+            {
+                lastCameraPosition = Input.mousePosition;
+            }
 
-        if (Input.GetMouseButton(1))
-        {
-            Vector3 delta = Input.mousePosition - lastCameraPosition;
-            transform.Translate(0, delta.y * -sensitivity, 0);
-            lastCameraPosition = Input.mousePosition;
+            if (Input.GetMouseButton(1))
+            {
+                Vector3 delta = Input.mousePosition - lastCameraPosition;
+                transform.Translate(0, delta.y * -sensitivity, 0);
+                lastCameraPosition = Input.mousePosition;
+            }
         }
     }
 }
