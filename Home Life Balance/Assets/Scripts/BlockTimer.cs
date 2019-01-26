327d9bff4470b49fe235bd55275f263da6dada03
Wiley <jthom330@gmail.com>
2019-01-26 01:57:15 -0500

    Added height check logic

---
+++ Home Life Balance/Assets/Scripts/BlockTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTimer : MonoBehaviour
+{
+    public float countDown = 10;
+    private float currentTime = 0;
+    private bool isAtHeight = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAtHeight)
+        {
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= countDown)
+            {
+                //spawn roof
+                Debug.Log("min height achieved");
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        isAtHeight = true;
+    }
+
+    public void StopTimer()
+    {
+        isAtHeight = false;
+        currentTime = 0;
+    }
+}
