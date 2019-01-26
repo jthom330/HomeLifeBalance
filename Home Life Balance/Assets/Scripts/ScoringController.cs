a3947d000aa25fc750198bf2b69abbf72b75fdaf
Brandon B <bruceb888@gmail.com>
2019-01-26 15:21:18 -0500

    Adding basic scoring controller

---
+++ Home Life Balance/Assets/Scripts/ScoringController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringController : MonoBehaviour
+{
+    public int objectValue = 10;
+    private bool scoreApplied = false;
+    List<GameObject> currentCollisions = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentCollisions.Count > 0 && !scoreApplied)
+        {
+            ScoreManager.score += objectValue;
+            scoreApplied = true;
+        }
+        if (currentCollisions.Count <= 0 && scoreApplied)
+        {
+            ScoreManager.score -= objectValue;
+            scoreApplied = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlacedBlock")
+        {
+            currentCollisions.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlacedBlock")
+        {
+            currentCollisions.Remove(collision.gameObject);
+        }
+    }
+}
