1b6dc79c65f573c0ede6e6d2ee898fa4092a3fa6
Wiley <jthom330@gmail.com>
2019-01-25 23:29:13 -0500

    Added drag script and basic prefab

---
+++ Home Life Balance/Assets/Scripts/DragBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragBehavior : MonoBehaviour
+{
+
+    public float localGravityScale = 1;
+    public float speed = 10;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    private void OnMouseDrag()
+    {
+        var pos = Input.mousePosition;        
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        pos.z = 0;
+
+        transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+    }
+
+    private void OnMouseUp()
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = localGravityScale;
+    }
+}
