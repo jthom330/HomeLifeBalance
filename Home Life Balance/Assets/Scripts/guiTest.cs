50d28a6ec17f62b5ef2ea2c944261f61b401c47c
Nocoa <nocoa@gmx.com>
2019-01-25 22:50:14 -0500

    Add guiTest.cs and square sprite

---
+++ Home Life Balance/Assets/Scripts/guiTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guiTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnGUI()
+    {
+        if (Application.isEditor)
+        {
+            GUI.skin.label.wordWrap = false;
+            GUI.skin.label.clipping = 0;
+            GUI.Label(new Rect(transform.position.x, transform.position.y, 100, 100), string.Format("{0}", "test"));
+        }
+    }
+}
