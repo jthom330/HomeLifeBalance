8af6d9b26313c1e006695596e32cbf247f7fb502
Wiley <jthom330@gmail.com>
2019-01-26 12:28:19 -0500

    Added destruction logic and updated min height rule.

---
+++ Home Life Balance/Assets/Scripts/DestructionBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionBehavior : MonoBehaviour
+{
+    public GameObject destroyEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "PlacedBlock")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            collision.gameObject.GetComponent<Collider2D>().enabled = false;
+            ContactPoint2D contact = collision.contacts[0];
+            StartCoroutine(AnimateAndDestroy(contact.point, collision.gameObject));
+        }
+    }
+
+    IEnumerator AnimateAndDestroy(Vector2 point, GameObject block)
+    {
+        GameObject instance = Instantiate(destroyEffect, block.transform.position, Quaternion.identity);
+        instance.transform.localScale = Vector2.zero;
+        float ElapsedTime = 0.0f;
+        float TotalTime = 0.5f;
+        while (ElapsedTime < TotalTime)
+        {
+            ElapsedTime += Time.deltaTime;
+            instance.transform.localScale = Vector2.Lerp(instance.transform.localScale, Vector2.one, (ElapsedTime / TotalTime));
+            yield return null;
+        }
+
+        Destroy(block);
+        ElapsedTime = 0.0f;
+
+        while (ElapsedTime < TotalTime)
+        {
+            ElapsedTime += Time.deltaTime;
+            instance.transform.localScale = Vector2.Lerp(instance.transform.localScale, Vector2.zero, (ElapsedTime / TotalTime));
+            yield return null;
+        }
+
+        Destroy(instance);
+    }
+}
--- Home Life Balance/Assets/Scripts/HeightCheck.cs
+++ Home Life Balance/Assets/Scripts/HeightCheck.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeightCheck : MonoBehaviour
-{  
+{
+
+    public float resetTimer = 10f;
+
+    private void Update()
+    {
+        resetTimer -= Time.deltaTime;
+        if (resetTimer <= 0)
+        {
+            //remove roof option 
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        resetTimer = 10f;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "PlacedBlock")
