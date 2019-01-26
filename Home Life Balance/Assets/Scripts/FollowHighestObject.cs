a32cd05c5275df0af707e042042062047059b819
Brandon B <bruceb888@gmail.com>
2019-01-26 04:23:05 -0500

    Added zoom logic to camera script

--- Home Life Balance/Assets/Scripts/FollowHighestObject.cs
+++ Home Life Balance/Assets/Scripts/FollowHighestObject.cs
@@ -5,22 +5,49 @@ using System.Linq;
 
 public class FollowHighestObject : MonoBehaviour
 {
-    public string tag;
+    public string objectTag;
     public float cameraSpeed;
+    public float verticalOffset;
+    public float startingZoomDistance;
+    public float maxZoomDistance;
+    public float zoomFactor;
+
     private Vector3 newCameraPos;
+    private float currentZoomDistance;
+    private float newZoomDistance;
     private GameObject highestGameObject;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        this.GetComponent<Camera>().orthographicSize = startingZoomDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        highestGameObject = GameObject.FindGameObjectsWithTag(tag).OrderBy(highestGameObject => highestGameObject.transform.position.y).Last();
-        newCameraPos = new Vector3(highestGameObject.transform.position.x, highestGameObject.transform.position.y, transform.position.z);
+        AdjustCameraZoom();
+        AdjustCameraPosition();
+    }
+
+    void AdjustCameraZoom()
+    {
+        currentZoomDistance = this.GetComponent<Camera>().orthographicSize;
+        newZoomDistance = startingZoomDistance + (newCameraPos.y * zoomFactor);
+        if (newZoomDistance > maxZoomDistance)
+        {
+            newZoomDistance = maxZoomDistance;
+        }
+        if (newZoomDistance < startingZoomDistance)
+        {
+            newZoomDistance = startingZoomDistance;
+        }
+        this.GetComponent<Camera>().orthographicSize = Mathf.Lerp(currentZoomDistance, newZoomDistance, cameraSpeed * Time.deltaTime);
+    }
+
+    void AdjustCameraPosition() {
+        highestGameObject = GameObject.FindGameObjectsWithTag(objectTag).OrderBy(highestGameObject => highestGameObject.transform.position.y).Last();
+        newCameraPos = new Vector3(highestGameObject.transform.position.x, highestGameObject.transform.position.y + verticalOffset, transform.position.z);
         this.transform.position = Vector3.Lerp(transform.position, newCameraPos, cameraSpeed * Time.deltaTime);
     }
 }
