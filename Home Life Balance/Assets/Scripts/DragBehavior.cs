f40046bd9ae4153de2b1e48988843a8e1d26b985
Wiley <jthom330@gmail.com>
2019-01-25 23:41:05 -0500

    Drag script now disables after being droped

--- Home Life Balance/Assets/Scripts/DragBehavior.cs
+++ Home Life Balance/Assets/Scripts/DragBehavior.cs
@@ -7,35 +7,40 @@ public class DragBehavior : MonoBehaviour
 
     public float localGravityScale = 1;
     public float speed = 10;
-    // Start is called before the first frame update
+    public bool pickup = true;
 
+    // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnMouseDown()
     {
-        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        if (pickup)
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        }
     }
 
     private void OnMouseDrag()
     {
-        var pos = Input.mousePosition;        
-        pos = Camera.main.ScreenToWorldPoint(pos);
-        pos.z = 0;
+        if (pickup)
+        {
+            var pos = Input.mousePosition;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            pos.z = 0;
 
-        transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+        }
     }
 
     private void OnMouseUp()
     {
-        gameObject.GetComponent<Rigidbody2D>().gravityScale = localGravityScale;
+        if (pickup)
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = localGravityScale;
+            pickup = false;
+        }
     }
 }
