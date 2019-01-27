2e19fbc669e4b2232575def22c5e54219bab03a9
Wiley <jthom330@gmail.com>
2019-01-27 14:47:45 -0500

    Added multipliers and have base for win condition.  Cleaned up scene.

---
+++ Home Life Balance/Assets/Scripts/RoofWin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofWin : MonoBehaviour
+{
+    public float winCountDown = 10;
+    private float currentTime = 0;
+
+    private void Update()
+    {
+        if(currentTime >= winCountDown)
+        {
+            // Win 
+            Debug.Log("Win!");
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "PlacedBlock")
+        {
+            currentTime += Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlacedBlock")
+        {
+            currentTime = 0;
+        }
+    }
+}
