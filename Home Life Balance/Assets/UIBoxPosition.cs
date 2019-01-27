7d3c5294d8a2f1c9d44549d1618ab85cb62699b5
Brandon B <bruceb888@gmail.com>
2019-01-26 22:38:02 -0500

    Fixing uiBox scaling/position issues

---
+++ Home Life Balance/Assets/UIBoxPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBoxPosition : MonoBehaviour
+{
+    public Camera camera;
+    public float offsetx = 0;
+    public float offsety = 0;
+    private Vector3 startingScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingScale = transform.localScale;
+        //startingCameraSize = camera.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = camera.ViewportToWorldPoint(new Vector2(.5f + offsetx, 1f-offsety));
+        newPos.z = 10;
+        transform.position = newPos;
+        transform.localScale = new Vector2(camera.orthographicSize/ startingScale.x, camera.orthographicSize/ startingScale.y);
+    }
+}
